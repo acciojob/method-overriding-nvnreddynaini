ea075dfa1fad8be14baa419a5f4d870e32ecad11
VN Reddy Naini <vnreddynaini@VNs-MacBook-Air.local>
2022-12-12 05:28:25 +0530

    method overriding 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,18 @@
 package com.driver;
 
 public class Main {
-  public class A{
-      public String meth(){
-          return "Invoking method from class A";
-      }
-  }
-  public class B extends A{
-      public String meth(){
-          return "Method is overridden in Extendend class B";
-      }
-  }
-  public void main(String args[]){
+  public static void main(String args[]){
       B obj = new B();
       obj.meth();
   }
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
